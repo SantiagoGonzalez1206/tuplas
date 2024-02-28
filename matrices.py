8c6a46c5f2d0b0213c9dfb5bd4a35d6ccf2161b0
SantiagoGonzalez1206 <160767943+SantiagoGonzalez1206@users.noreply.github.com>
2024-02-28 09:29:12 -0500

    Estudiar

---
+++ matrices.py
@@ -0,0 +1,12 @@
+list = [34, "sentidos", True, 8898]
+print(list)
+list [3] = 889
+print(list)
+list.append("contra")
+print(list)
+list.append(34)
+print(list)
+print(list.count(34))
+print(list.index("sentidos"))
+list.remove(True)
+print(list)
